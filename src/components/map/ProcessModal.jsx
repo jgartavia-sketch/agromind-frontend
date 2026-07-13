0d440de370c69f709e591f4fffbd8ac3b690da6a
TU_NOMBRE <jgartavia@gmail.com>
2026-07-13 12:44:33 -0600

    fix(rbac): correct FarmContext import in ProcessModal

--- src/components/map/ProcessModal.jsx
+++ src/components/map/ProcessModal.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useMemo, useState } from "react";
-import { useFarm } from "../context/FarmContext";
+import { useFarm } from "../../context/FarmContext";
 
 function nowIso() {
   return new Date().toISOString();
